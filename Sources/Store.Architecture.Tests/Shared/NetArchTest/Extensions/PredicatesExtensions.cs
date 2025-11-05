7b81c1ac91f919e54bd45683fab7ff5849780fb3
Doru <doru.zanet@live.com>
2025-11-05 09:53:42 +0200

    added documentation and extended tests (#12)

--- Sources/Store.Architecture.Tests/Shared/NetArchTest/Extensions/PredicatesExtensions.cs
+++
@@ -1,9 +0,0 @@
-﻿using Mono.Cecil;
-
-namespace Store.Architecture.Tests;
-
-internal static class PredicatesExtensions
-{
-    public static PredicateList MeetCustomRule(this Predicates conditions, Func<TypeDefinition, bool> typeCheck) 
-        => conditions.MeetCustomRule(new GenericCustomRule(typeCheck));
-}
