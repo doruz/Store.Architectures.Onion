e9ba264f03d39745ec4c617cc488fe6e4cbeb8ac
Doru Zanet <doru.zanet@live.com>
2025-11-13 14:36:26 +0200

    extended fitness tests for domain entities and values

--- Sources/Store.Tests/Core/DomainLayerTests.cs
+++ Sources/Store.Tests/Core/DomainLayerTests.cs
@@ -18,13 +18,26 @@ public class DomainLayerTests
     }
 
     [Fact]
+    public void DomainEntitiesAndValues_Should_NotHavePropertiesSettersExposedExternally()
+    {
+        var result = SolutionTypes.Core.Domain
+            .That()
+            .ResideInNamespace($"{SolutionNamespaces.Core.Domain}.Entities").And().DoNotHaveName(nameof(BaseEntity))
+            .Should()
+            .HaveAllPropertiesWithoutPublicSetters()
+            .GetResult();
+
+        result.FailingTypeNames.Should().BeNullOrEmpty();
+    }
+
+    [Fact]
     public void DomainValues_Should_BePublicImmutableRecords()
     {
         var result = SolutionTypes.Core.Domain
             .That()
             .AreRecords()
             .Should()
-            .BePublic().And().HaveOnlyInitProperties()
+            .BePublic().And().HaveAllPropertiesWithInitOnly()
             .GetResult();
 
         result.FailingTypeNames.Should().BeNullOrEmpty();
